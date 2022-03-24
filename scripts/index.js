9f88d781a9cf67f90fe2cb878e005d71b3bb7d9b
Ilya Ivanov <ilushkaivanov@mail.ru>
2022-03-24 11:54:44 +0300

    bugfix/edited-version v.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,36 @@
 // Попап контейнер-форма и ее обработчик отправки
 let formElement = document.querySelector('.popup__container');
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    profileTitle.textContent=popupInput[0].value;
-    profileSubtitle.textContent=popupInput[1].value;
-    popup.classList.remove('popup_opened');
-}
 // Блок попапа с затемнением
 let popup = document.querySelector('.popup');
 // Переменные DOM-элементов для ввода данных попапа
-let popupInput = document.querySelectorAll('.popup__input');
-let nameInput = popupInput[0];
-let jobInput = popupInput[1];
+let nameInput = document.querySelector('.popup__input_type_title');
+let jobInput = document.querySelector('.popup__input_type_subtitle');
 // Текстовые элементы блока profile
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 // Кнопка изменения данных, открытия попапа и ее обработчик нажатия
 let profileEditButton = document.querySelector('.profile__button_type_edit');
-
-function profileEditButtonClickHandler(){
-    popupInput[0].value = profileTitle.textContent;
-    popupInput[1].value = profileSubtitle.textContent;
-    popup.classList.add('popup_opened');
-}
 // Кнопка закрытия попапа и ее обработчик нажатия
 let popupCloseButton = document.querySelector('.popup__button_type_close');
+
+function profileEditButtonClickHandler(){
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+    popup.classList.add('popup_opened');
+}
 
 function popupCloseButtonClickHandler(){
     popup.classList.remove('popup_opened');
 }
 
+function submitFormHandler (evt) {
+    evt.preventDefault();
+    profileTitle.textContent=nameInput.value;
+    profileSubtitle.textContent=jobInput.value;
+    popupCloseButtonClickHandler();
+}
+
 // Добавление слушателей событий
 popupCloseButton.addEventListener('click', popupCloseButtonClickHandler)
 profileEditButton.addEventListener('click', profileEditButtonClickHandler);
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', submitFormHandler);
